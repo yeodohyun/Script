0862998b73574c258e4c6513d958d2dbb3be2218
Eodkfdl <333wmf@naver.com>
2016-05-31 02:35:35 +0900

    연락처검색및 출력완료
    페이지 구간설정으로 전체탐색하게하고 연락처검색을해서 출력을 할수있게 만들었습니다.

--- 과제.py
+++ 과제.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 30 17:10:12 2016
@@ -15,11 +15,11 @@ BooksDoc = None
 
 ##### getdata class
 class GetData:
-    
+    페이지설정="&numOfRows=999&pageSize=999&pageNo=1&startPage=1"
     key = 'peuzoABFl3ew9WDJeae1ap8n5rlnIok9P1zH0%2FzIXXz2LM%2B8qahwkE1WckPkvD%2FET%2BZ5nN3LltIICJNplE0zvA%3D%3D'
-    여행경보url = "http://apis.data.go.kr/1262000/TravelWarningService/getTravelWarningList?ServiceKey=" + key + '&numOfRows=999&pageSize=999&pageNo=1&startPage=1'
-    국가별공지사항url = "http://openapi.0404.go.kr/openapi/service/CountryNoticeService/getCountryNoticeList?ServiceKey=" + key + '&numOfRows=999&pageSize=999&pageNo=1&startPage=1'
-    연락처정보url="http://apis.data.go.kr/1262000/ContactService/getContactList?serviceKey="+key
+    여행경보url = "http://apis.data.go.kr/1262000/TravelWarningService/getTravelWarningList?ServiceKey=" + key + '&numOfRows=999&pageSize=999&pageNo=1&startPage=1'+페이지설정
+    국가별공지사항url = "http://openapi.0404.go.kr/openapi/service/CountryNoticeService/getCountryNoticeList?ServiceKey=" + key + 페이지설정
+    연락처정보url="http://apis.data.go.kr/1262000/ContactService/getContactList?serviceKey="+key+페이지설정
     def main(self):
         여행경보data = urllib.request.urlopen(self.여행경보url).read()        
         f = open("여행경보제도.xml","wb")
@@ -124,24 +124,15 @@ def Print국가별공지사항():
 def Print찾는나라연락처(나라이름):
 #   
     tree = etree.parse('연락처정보.xml')
-  
-    root = tree.getroot()
-    txt=root.find('body').find('items').find('items')
     
- 
-#    for a in root.findall('body'):
-#        for b in a.findall('items'):
-#            for c in b.findall('items'):
-#                    print('attentionNote : ', c.findtext('id'))
-#                    print('attentionPartial : ', c.findtext('countryName'))
-#                    print('countryEnName : ', c.findtext('countryEnName'))              
-#                    print('continent : ', c.findtext('continent'))
-#                    print('controlNote : ', c.findtext('contact'))
-#                    print('controlPartial : ', c.findtext('controlPartial'))
-#                    print('wrtDt : ', c.findtext('wrtDt'))
-#                    print('국가국기이미지 : ', c.findtext('imgUrl'))
-#                    print('-------------------')
-  
+    root = tree.getroot()
+
+    for a in root.findall('body'):
+        for b in a.findall('items'):
+            for c in b.findall('item'):
+                if c.findtext('countryName')==나라이름:
+                    print(c.findtext('contact'))
+                   
                 
 def 지도url(지역):
     url= 'https://www.google.co.kr/maps/search/주+'+지역+'+대한민국+대사관'
@@ -153,142 +144,4 @@ while(loopFlag > 0):
     menuKey = str(input ('select menu :'))
     launcherFunction(menuKey)
 else:
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 30 17:10:12 2016
-
-@author: DH
-"""
-
-import urllib.request
-import xml.etree.ElementTree as etree
-##### global
-loopFlag = 1
-xmlFD = -1
-BooksDoc = None
-
-##### getdata class
-class GetData:
-    
-    key = 'peuzoABFl3ew9WDJeae1ap8n5rlnIok9P1zH0%2FzIXXz2LM%2B8qahwkE1WckPkvD%2FET%2BZ5nN3LltIICJNplE0zvA%3D%3D'
-    여행경보url = "http://apis.data.go.kr/1262000/TravelWarningService/getTravelWarningList?ServiceKey=" + key + '&numOfRows=999&pageSize=999&pageNo=1&startPage=1'
-    국가별공지사항url = "http://openapi.0404.go.kr/openapi/service/CountryNoticeService/getCountryNoticeList?ServiceKey=" + key + '&numOfRows=999&pageSize=999&pageNo=1&startPage=1'
-    연락처정보url="http://apis.data.go.kr/1262000/ContactService/getContactList?serviceKey="+key
-    def main(self):
-        여행경보data = urllib.request.urlopen(self.여행경보url).read()        
-        f = open("여행경보제도.xml","wb")
-        f.write(여행경보data)
-        f.close()
-        
-        국가별공지사항data = urllib.request.urlopen(self.국가별공지사항url).read()
-        f = open("국가별 공지사항 목록조회.xml","wb")
-        f.write(국가별공지사항data)
-        f.close()
-        
-        
-        연락처정보data = urllib.request.urlopen(self.연락처정보url).read()        
-        f = open("연락처정보.xml","wb")
-        f.write(연락처정보data)
-        f.close()
-        print ("XML Document loading complete")
-        
-#### Menu  implementation
-def printMenu():
-    print("\nWelcome! Book Manager Program (xml version)") 
-    print("========Menu==========")
-    print("Get data:  g")
-    print("Print title:   p")
-    print("Print 여행경보:   p1")
-    print("Print 국가별 공지사항 목록조회:   p2")
-    print("Quit program:   q")
-    print("==================")
-    
-def launcherFunction(menu):
-    global BooksDoc
-    global loopFlag
-    if menu ==  'g':
-        getData = GetData()
-        getData.main()
-    elif menu == 'q':
-        loopFlag = 0    
-    elif menu == 'p':
-        PrintTitle()
-    elif menu == 'p1':
-        Print여행경보()
-    elif menu == 'p2':
-        Print국가별공지사항()
-    elif menu == 'b':
-        PrintBookList(["title",])
-    elif menu == 'a':
-        ISBN = str(input ('insert ISBN :'))
-        title = str(input ('insert Title :'))
-        AddBook({'ISBN':ISBN, 'title':title})
-    elif menu == 'e':
-        keyword = str(input ('input keyword to search :'))
-        printBookList(SearchBookTitle(keyword))
-    elif menu == 'm':
-        keyword = str(input ('input keyword code to the html  :'))
-        html = MakeHtmlDoc(SearchBookTitle(keyword))
-        print("-----------------------")
-        print(html)
-        print("-----------------------")
-    else:
-        print ("error : unknow menu key")
-
-def PrintTitle():
-    
-    tree = etree.parse('국가별 공지사항 목록조회.xml')
-    root = tree.getroot()
-    
-    for a in root.findall('body'):
-        for b in a.findall('items'):
-            for c in b.findall('item'):
-                print('title : ', c.findtext('title'))
-              
-def Print여행경보():
-    
-    tree = etree.parse('여행경보제도.xml')
-    root = tree.getroot()
-    
-    for a in root.findall('body'):
-        for b in a.findall('items'):
-            for c in b.findall('item'):
-                print('attentionNote : ', c.findtext('attentionNote'))
-                print('attentionPartial : ', c.findtext('attentionPartial'))
-                print('continent : ', c.findtext('continent'))
-                print('controlNote : ', c.findtext('controlNote'))
-                print('controlPartial : ', c.findtext('controlPartial'))
-                print('countryEnName : ', c.findtext('countryEnName'))
-                print('countryName : ', c.findtext('countryName'))
-                print('id : ', c.findtext('id'))
-                print('imgUrl1 : ', c.findtext('imgUrl'))
-                print('imgUrl2 : ',c.findtext('imgUrl2'))
-                print('limitaNote : ', c.findtext('limitaNote'))
-                print('limitaPartial : ', c.findtext('limitaPartial'))
-                print('wrtDt : ', c.findtext('wrtDt'))
-                print('-------------------')
-                
-def Print국가별공지사항():
-    
-    tree = etree.parse('국가별 공지사항 목록조회.xml')
-    root = tree.getroot()
-    
-    for a in root.findall('body'):
-        for b in a.findall('items'):
-            for c in b.findall('item'):
-                print('title : ', c.findtext('title'))
-                print('fileUrl : ', c.findtext('fileUrl'))
-                print('id : ', c.findtext('id'))
-                print('wrtDt : ', c.findtext('wrtDt'))
-                print('-------------------')
-
-
-############### run ###############
-while(loopFlag > 0):
-    printMenu()
-    menuKey = str(input ('select menu :'))
-    launcherFunction(menuKey)
-else:
->>>>>>> origin/master
     print ("Thank you! Good Bye")
